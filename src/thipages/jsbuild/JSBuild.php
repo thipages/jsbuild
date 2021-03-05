229a9d7cba59e4a573a9952990e4bb704eea208d
thipages <thipages@gmail.com>
2021-03-05 11:27:48 +0100

    cleansing

--- src/thipages/jsbuild/JSBuild.php
+++ src/thipages/jsbuild/JSBuild.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace thipages\jsbuild;
-use thipages\jsbuild\Base64Config;
+use stdClass;
+
 class JSBuild {
     const MODEL=[
         'author'=>0,
@@ -10,26 +11,25 @@ class JSBuild {
         'description'=>0,
         'keywords'=>1
     ];
-    private $config;
+    private $fileName;
     private $root;
     public function __construct($root=null,$fileName="jsbuild.json") {
-        $root=self::thisDir($root);
-        $this->config = json_decode(file_get_contents($root."/".$fileName));
-        $this->root = $root;
+        $this->root=self::thisDir($root);
+        $this->fileName=$fileName;
     }
-    public function replaceSlot ($slot, $content) {
-        return str_replace('#'.$slot.'#',$this->config->{$slot},$content);
+    public function replaceSlot ($slot, $content,$config) {
+        return str_replace('#'.$slot.'#',$config->{$slot},$content);
     }
-    public function replaceSlot_list ($slot, $content) {
+    public function replaceSlot_list ($slot, $content, $config) {
         $l=[];
-        foreach ($this->config->{$slot} as $element) $l[]='"'.$element.'"';
+        foreach ($config->{$slot} as $element) $l[]='"'.$element.'"';
         return str_replace('"#'.$slot.'#"',join(',',$l),$content);
     }
     public static function thisDir($root) {
         return $root===null ? getcwd() : $root;
     }
     public static function writeBuildModel($root=null) {
-        $obj=new \stdClass();
+        $obj=new stdClass();
         foreach (self::MODEL as $field=>$type) {
             if ($type===0) {
                 $value='a'.ucfirst($field);
@@ -41,18 +41,18 @@ class JSBuild {
         }
         file_put_contents(self::thisDir($root).'/jsbuild.json',json_encode($obj,JSON_PRETTY_PRINT));
     }
-    public function replaceGenerics($target,$content) {
-        foreach (array_keys(get_object_vars($this->config)) as $slot) {
-            $content=is_array($this->config->{$slot})
-                ? $this->replaceSlot_list($slot,$content) 
-                : $this->replaceSlot($slot,$content);
+    public function replaceGenerics($target,$content, $config) {
+        foreach (array_keys(get_object_vars($config)) as $slot) {
+            $content=is_array($config->{$slot})
+                ? $this->replaceSlot_list($slot,$content,$config) 
+                : $this->replaceSlot($slot,$content,$config);
         }
         file_put_contents($target, $content);
     }
     public function writeBuild($echoing=true) {
+        $config = json_decode(file_get_contents($this->root."/".$this->fileName));
         $thisDir=$this->root;
         $targetRollupDir=$thisDir."/rollup/";
-        $sourceRollupDir=dirname(__FILE__)."/rollup/";
         $valid= (!file_exists( $targetRollupDir ) && !is_dir( $targetRollupDir ));
         //
         if ($valid) {
@@ -60,7 +60,7 @@ class JSBuild {
             foreach(Base64Config::getContent() as $item) {
                 $this->replaceGenerics(
                     join(DIRECTORY_SEPARATOR ,[$item[0],$item[1]]),
-                    base64_decode($item[2])
+                    base64_decode($item[2]),$config
                 );
             }
         }
